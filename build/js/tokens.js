e1e29392f85f13fa3f527ce17b381f9b2b8b9a4f
Rodrigo Oliveira <devv.rodrigo@gmail.com>
2023-11-15 09:52:27 +0000

    Init commit

---
+++ build/js/tokens.js
@@ -0,0 +1,18 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 15 Nov 2023 09:44:15 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray500": "#3f3f46",
+  "ColorGray700": "#18181b",
+  "ColorGray900": "#09090b",
+  "ColorGreen600": "#4d7c0f",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionGreen700": "#4d7c0fff",
+  "VariableCollectionGray500": "#3f3f46ff",
+  "VariableCollectionGray700": "#18181bff",
+  "VariableCollectionGray800": "#18181bff",
+  "VariableCollectionGray900": "#09090bff"
+};
